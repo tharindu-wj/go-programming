7641d629e89fb74df553fcbc394401dcac6935af
Tharindu Wickramasinghe <jhtw99@gmail.com>
2019-04-07 21:43:04 +0530

    forst exercise done

--- code_samples/012-updated-version/009a-intro-to-packages/main.go
+++ code_samples/012-updated-version/009a-intro-to-packages/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	n, _ := fmt.Println("Hello, playground", 42, true)
+	n, e := fmt.Println("Hello, playground", 42, true)
 	fmt.Println(n)
+	fmt.Println(e)
 }
